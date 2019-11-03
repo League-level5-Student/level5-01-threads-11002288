e55550cb700860f1bef3d16e7973819cb24a9989
League Student <league@iMac-40.attlocal.net>
2019-11-03 11:29:29 -0800

    asdf
    asdf

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,5 +1,6 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
@@ -9,36 +10,57 @@ public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One
 	// robot should draw one ring simultaneously with the other 4 robots.
 	public static void main(String[] args) {
-		Robot t1 = new Robot(400, 500);
-		Robot t2 = new Robot(800, 500);
-		Robot t3 = new Robot(1200, 500);
-		Robot b1 = new Robot(600, 700);
-		Robot b2 = new Robot(1000, 700);
-		b2.setPenWidth(100);
-		b2.setRandomPenColor();
-		b2.hide();
-		Thread m1 = new Thread(() ->t1.move(360));
-		Thread m2 = new Thread(() -> t2.move(360));
-		Thread m3 = new Thread(() ->t3.move(360));
-		Thread m4 = new Thread(() ->b1.move(360));
-		Thread m5 = new Thread(() ->{b2.penDown();b2.move(100); b2.penUp(); });
-	
-		Thread tu1 = new Thread(() -> t1.turn(360));
-		Thread tu2 = new Thread(() ->t2.turn(360));
-		Thread tu3 = new Thread(() ->t3.turn(360));
-		Thread tu4 = new Thread(() ->b1.turn(360));
-		Thread tu5 = new Thread(() ->{b2.turn(360);});
-
+		Robot t1 = new Robot(10, 320);
+		Robot t2 = new Robot(510, 320);
+		Robot t3 = new Robot(1010, 320);
+		Robot b1 = new Robot(270, 700);
+		Robot b2 = new Robot(750, 700);
+		t1.setPenColor(Color.BLUE);
+		t3.setPenColor(Color.RED);
+		b1.setPenColor(Color.YELLOW);
+		b2.setPenColor(Color.GREEN);
+		t1.setSpeed(10);
+		t2.setSpeed(10);
+		t3.setSpeed(10);
+		b1.setSpeed(10);
+		b2.setSpeed(10);
+		
+		Thread m1 = new Thread(() -> {for (int i = 0; i < 72; i++) {
+			t1.penDown();
+			t1.move(25);
+			t1.turn(5);
+		}});
+		Thread m2 = new Thread(() -> {for (int i = 0; i < 72; i++) {
+			t2.penDown();
+			t2.move(25);
+			t2.turn(5);
+		}});
+		Thread m3 = new Thread(() ->{for (int i = 0; i < 72; i++) {
+			t3.penDown();
+			t3.move(25);
+			t3.turn(5);
+		}});
+		Thread m4 = new Thread(() ->{for (int i = 0; i < 72; i++) {
+			b1.penDown();
+			b1.move(25);
+			b1.turn(5);
+		}});
+		Thread m5 = new Thread(() ->{for (int i = 0; i < 72; i++) {
+			b2.penDown();
+			b2.move(25);
+			b2.turn(5);
+		}});
+		
 		m1.start();
-		tu1.start();
+		//tu1.start();
 		m2.start();
-		tu2.start();
+		//tu2.start();
 		m3.start();
-		tu3.start();
+		//tu3.start();
 		m4.start();
-		tu4.start();
+		//tu4.start();
 		m5.start();
-		tu5.start();
+		//tu5.start();
 
 	}
 }
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,60 @@
 package _02_Advanced_Robot_Race;
 
+import java.awt.Color;
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		Robot t1 = new Robot(100, 900);
+		Robot t2 = new Robot(500, 900);
+		Robot t3 = new Robot(900, 900);
+		Robot b1 = new Robot(300, 900);
+		Robot b2 = new Robot(700, 900);
+		t1.setPenColor(Color.BLUE);
+		t3.setPenColor(Color.RED);
+		b1.setPenColor(Color.YELLOW);
+		b2.setPenColor(Color.GREEN);
+		t1.setSpeed(5);
+		t2.setSpeed(5);
+		t3.setSpeed(5);
+		b1.setSpeed(5);
+		b2.setSpeed(5);
+		Random ram = new Random();
+		Thread m1 = new Thread(() -> {while (t1.getY() != 100) {
+			t1.setSpeed(ram.nextInt(10));
+			t1.move(300);
+		}});
+		Thread m2 = new Thread(() -> {while (t2.getY() != 100) {
+			t2.setSpeed(ram.nextInt(10));
+			t2.move(300);
+		}});
+		Thread m3 = new Thread(() ->{while (t3.getY() != 100) {
+			t3.setSpeed(ram.nextInt(10));
+			t3.move(300);
+		}});
+		Thread m4 = new Thread(() ->{while (b1.getY() != 100) {
+			b1.setSpeed(ram.nextInt(10));
+			b1.move(300);
+		}});
+		Thread m5 = new Thread(() ->{while (b2.getY() != 100) {
+			b2.setSpeed(ram.nextInt(10));
+			b2.move(300);
+		}});
+		
+		m1.start();
+		//tu1.start();
+		m2.start();
+		//tu2.start();
+		m3.start();
+		//tu3.start();
+		m4.start();
+		//tu4.start();
+		m5.start();
+		//tu5.start();
+
+	}
 }
